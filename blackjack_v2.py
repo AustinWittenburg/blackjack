164de5a99cdf1449b2d1f681dc2c146009344be3
AustinWittenburg <wittenburg7@gmail.com>
2023-05-26 15:57:58 -0500

    Update blackjack_v2.py

--- blackjack_v2.py
+++ blackjack_v2.py
@@ -1,3 +1,44 @@
 import random
 
+freshDeck = ['2♣', '3♣', '4♣', '5♣', '6♣', '7♣', '8♣', '9♣', 'T♣', 'J♣', 'Q♣', 'K♣', 'A♣',
+             '2♥', '3♥', '4♥', '5♥', '6♥', '7♥', '8♥', '9♥', 'T♥', 'J♥', 'Q♥', 'K♥', 'A♥',
+             '2♠', '3♠', '4♠', '5♠', '6♠', '7♠', '8♠', '9♠', 'T♠', 'J♠', 'Q♠', 'K♠', 'A♠',
+             '2♦', '3♦', '4♦', '5♦', '6♦', '7♦', '8♦', '9♦', 'T♦', 'J♦', 'Q♦', 'K♦', 'A♦']
+deck = []
+HiLo = [ ['2', '3', '4', '5', '6'], ['7', '8', '9'], ['T', 'J', 'Q', 'K', 'A'] ]
+playerOptions = ["Hit", "Stand", "Double", "Split", "Surrender"]
+playerHands = [] # [ [card0, card1], [card0, card1] ] Multiple hands are optional
+dealerHand  = [] #   [card0, card1]
+
+numDecks = 5
+
+def shuffleDeck():
+    global deck
+    for _ in range(numDecks):
+        deck += freshDeck
+    random.shuffle(deck)
+
+def HiLoCount(card):
+    global count
+    global HiLo
+    if card[0] in HiLo[0]:
+        count += 1
+    elif card[0] in HiLo[2]:
+        count -= 1
+
+def drawCard(DealerDownCard):
+    card = deck.pop()
+    if not DealerDownCard:
+        HiLoCount(card)
+    return card
+
+def dealHands():
+    playerHands.append(drawCard(True) )
+    dealerHand .append(drawCard(False) ) # Don't update count on dealers down card
+    playerHands.append(drawCard(True) )
+    dealerHand .append(drawCard(True) )
+
+def hit(hand):
+    hand.append(drawCard(True) )
+
 
